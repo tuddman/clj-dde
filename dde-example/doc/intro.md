460270c7033c7aecf56a789dab53dc46985917b3
tuddman <scott.tudd@gmail.com>
2013-12-08 19:32:33 -0500

    file cleanup

---
+++ dde-example/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dde-example
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ dde-example/src/dde_example/core.clj
@@ -0,0 +1,13 @@
+(ns dde-example.core
+  (:require [clj-dde :refer :all]))
+
+ #_(use 'clj-dde.core)
+
+  (def mt4 (conversation))
+  (type mt4)
+
+  (setTimeout mt4 2000)
+
+  (connect mt4 "MT4" "BID")
+
+
