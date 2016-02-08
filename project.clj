960e194a2f63a27ea5c09d9bad30abd49219421a
shaolang <shaolang@ymail.com>
2016-02-08 13:41:36 +0800

    Bump up version for development

--- project.clj
+++ project.clj
@@ -1,13 +1,7 @@
-(defproject clj-dde "0.1.2"
+(defproject clj-dde "0.2.0-dev"
   :description "clj-dde: for when you need to connect to a DDE (dynamic data exchange) source"
   :url "http://github.com/tuddman/clj-dde"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :java-source-paths ["src/java"])
-
-
-
-
-
-
